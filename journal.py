0d99c1d823ca39a5d5c12613c9daa27d01ab56e2
Muoi Ly <muoi.ly@gmail.com>
2015-07-05 00:56:03 -0700

    added authentication functionality to journal.py and test_journal.py

--- journal.py
+++ journal.py
@@ -3,26 +3,35 @@ from __future__ import unicode_literals
 import os
 import datetime
 import sqlalchemy as sa
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from pyramid.httpexceptions import HTTPFound
+from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm import scoped_session, sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 from pyramid.config import Configurator
 from pyramid.view import view_config
 from waitress import serve
+from cryptacular.bcrypt import BCRYPTPasswordManager
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 
 Base = declarative_base()
-DATABASE_URL = os.environ.get('DATABASE_URL','postgresql://muoily@localhost:5432/learning-journal')
+DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://muoily@localhost:5432/learning-journal')
 
-#Definiton of entries table
+
+# Definiton of entries table
 class Entry(Base):
     __tablename__ = 'entries'
-    id = sa.Column(sa.Integer, primary_key = True, autoincrement=True)
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     title = sa.Column(sa.Unicode(127), nullable=False)
     text = sa.Column(sa.UnicodeText, nullable=False)
-    created = sa.Column(sa.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    created = sa.Column(sa.DateTime, nullable=False,
+                        default=datetime.datetime.utcnow)
 
     @classmethod
     def write(cls, title=None, text=None, session=None):
@@ -39,15 +48,47 @@ class Entry(Base):
         return session.query(cls).order_by(cls.created.desc()).all()
 
 
-#function to initialize the database
+# function to initialize the database
 def init_db():
     engine = sa.create_engine(DATABASE_URL)
     Base.metadata.create_all(engine)
+
 
 @view_config(route_name='home', renderer='templates/list.jinja2')
 def home(request):
     entries = Entry.all()
     return {'entries': entries}
+
+
+@view_config(route_name='add', request_method='POST')
+def add_entry(request):
+    title = request.params.get('title')
+    text = request.params.get('text')
+    Entry.write(title=title, text=text)
+    return HTTPFound(request.route_url('home'))
+
+
+@view_config(context=DBAPIError)
+def db_exception(context, request):
+    from pyramid.response import Response
+    response = Response(context.message)
+    response.status_int = 500
+    return response
+
+
+def do_login(request):
+    username = request.params.get('username', None)
+    password = request.params.get('password', None)
+    if not (username and password):
+        raise ValueError('both username and password are required')
+
+    settings = request.registry.settings
+    manager = BCRYPTPasswordManager()
+    if username == settings.get('auth.username', ''):
+        hashed = settings.get('auth.password', '')
+        return manager.check(hashed, password)
+    return False
+
 
 def main():
     """Create a configured wsgi app"""
@@ -55,16 +96,29 @@ def main():
     debug = os.environ.get('DEBUG', True)
     settings['reload_all'] = debug
     settings['debug_all'] = debug
+    settings['auth.username'] = os.environ.get('AUTH_USERNAME', 'admin')
+    manager = BCRYPTPasswordManager()
+    settings['auth.password'] = os.environ.get(
+        'AUTH_PASSWORD', manager.encode('secret'))
     if not os.environ.get('TESTING', False):
+        # only bind the session if we are not testing
         engine = sa.create_engine(DATABASE_URL)
         DBSession.configure(bind=engine)
+    # add a secret value for auth tkt signing
+    auth_secret = os.environ.get('JOURNAL_AUTH_SECRET', 'itsaseekrit')
     # configuration setup
     config = Configurator(
-        settings=settings
+        settings=settings,
+        authentication_policy=AuthTktAuthenticationPolicy(
+            secret=auth_secret,
+            hashalg='sha512'
+        ),
+        authorization_policy=ACLAuthorizationPolicy(),
     )
     config.include('pyramid_tm')
     config.include('pyramid_jinja2')
     config.add_route('home', '/')
+    config.add_route('add', '/add')
     config.scan()
     app = config.make_wsgi_app()
     return app
--- test_journal.py
+++ test_journal.py
@@ -1,10 +1,11 @@
-#-*_ coding: utf-8 _*-
+# -*_ coding: utf-8 _*-
 from __future__ import unicode_literals
 import os
 import pytest
-
+from pyramid import testing
 from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
+from cryptacular.bcrypt import BCRYPTPasswordManager
 
 TEST_DATABASE_URL = os.environ.get(
     'DATABASE_URL',
@@ -15,6 +16,7 @@ os.environ['DATABASE_URL'] = TEST_DATABASE_URL
 os.environ['TESTING'] = "True"
 
 import journal
+
 
 @pytest.fixture(scope='session')
 def connection(request):
@@ -27,6 +29,7 @@ def connection(request):
     request.addfinalizer(journal.Base.metadata.drop_all)
     return connection
 
+
 @pytest.fixture()
 def db_session(request, connection):
     from transaction import abort
@@ -37,12 +40,14 @@ def db_session(request, connection):
     from journal import DBSession
     return DBSession
 
+
 @pytest.fixture()
 def app():
     from journal import main
     from webtest import TestApp
     app = main()
     return TestApp(app)
+
 
 @pytest.fixture()
 def entry(db_session):
@@ -54,53 +59,78 @@ def entry(db_session):
     db_session.flush()
     return entry
 
+
+@pytest.fixture(scope='function')
+def auth_req(request):
+    manager = BCRYPTPasswordManager()
+    settings = {
+        'auth.username': 'admin',
+        'auth.password': manager.encode('secret'),
+    }
+
+    testing.setUp(settings=settings)
+    req = testing.DummyRequest()
+
+    def cleanup():
+        testing.tearDown()
+
+    request.addfinalizer(cleanup)
+
+    return req
+
+
+
 def test_write_entry(db_session):
     kwargs = {'title': "Test Title", 'text': "Test entry text"}
     kwargs['session'] = db_session
-    #assert that there are no entries in the database:
+    # assert that there are no entries in the database:
     assert db_session.query(journal.Entry).count() == 0
-    #create an entry using the 'write' class method
+    # create an entry using the 'write' class method
     entry = journal.Entry.write(**kwargs)
-    #the entry we get back should be an instance of Entry
+    # the entry we get back should be an instance of Entry
     assert isinstance(entry, journal.Entry)
-    #id and created are generated automatically, but only on writing to 
-    #the database
+    # id and created are generated automatically, but only on writing to
+    # the database
     auto_fields = ['id', 'created']
     for field in auto_fields:
         assert getattr(entry, field, None) is None
 
     # flush the session to "write" the data to the database
     db_session.flush()
-    #now, we should have one entry
+    # now, we should have one entry
     assert db_session.query(journal.Entry).count() == 1
     for field in kwargs:
         if field != 'session':
             assert getattr(entry, field, '') == kwargs[field]
 
-    #id and created should be set automatically upon writing to db:
+    # id and created should be set automatically upon writing to db:
     for auto in ['id', 'created']:
-        assert getattr(entry, auto, None) is not None  
+        assert getattr(entry, auto, None) is not None
+
 
 def test_entry_no_title_fails(db_session):
-    bad_data = {'text':'test text'}
+    bad_data = {'text': 'test text'}
     journal.Entry.write(session=db_session, **bad_data)
     with pytest.raises(IntegrityError):
         db_session.flush()
 
+
 def test_entry_no_text_fails(db_session):
-    bad_data = {'title':'test title'}
+    bad_data = {'title': 'test title'}
     journal.Entry.write(session=db_session, *bad_data)
     with pytest.raises(IntegrityError):
         db_session.flush()
+
 
 def test_read_entries_empty(db_session):
     entries = journal.Entry.all()
     assert len(entries) == 0
 
+
 def test_read_entries_one(db_session):
     title_template = "Title {}"
     text_template = "Entry Text {}"
-    #write three entries, with order clear in the title and text
+    # write three entries, with order clear in the title and text
     for x in range(3):
         journal.Entry.write(
             title=title_template.format(x),
@@ -113,13 +143,15 @@ def test_read_entries_one(db_session):
     for entry in entries:
         assert isinstance(entry, journal.Entry)
 
+
 def test_empty_listing(app):
     response = app.get('/')
     assert response.status_code == 200
     actual = response.body
     expected = 'No entries here so far'
     assert expected in actual
-        
+
+
 def test_listing(app, entry):
     response = app.get('/')
     assert response.status_code == 200
@@ -127,3 +159,47 @@ def test_listing(app, entry):
     for field in ['title', 'text']:
         expected = getattr(entry, field, 'absent')
         assert expected in actual
+
+
+def test_post_to_add_view(app):
+    entry_data = {
+        'title': 'Hello there',
+        'text': 'This is a post',
+    }
+    response = app.post('/add', params=entry_data, status='3*')
+    redirected = response.follow()
+    actual = redirected.body
+    for expected in entry_data.values():
+        assert expected in actual
+
+
+def test_add_no_params(app):
+    response = app.post('/add', status=500)
+    assert 'IntegrityError' in response.body
+
+
+def test_do_login_success(auth_req):
+    from journal import do_login
+    auth_req.params = {'username': 'admin', 'password': 'secret'}
+    assert do_login(auth_req)
+
+
+def test_do_login_bad_pass(auth_req):
+    from journal import do_login
+    auth_req.params = {'username': 'admin', 'password': 'wrong'}
+    assert not do_login(auth_req)
+
+
+def test_do_login_bad_user(auth_req):
+    from journal import do_login
+    auth_req.params = {'username': 'bad', 'password': 'secret'}
+    assert not do_login(auth_req)
+
+
+def test_do_login_missing_params(auth_req):
+    from journal import do_login
+    for params in ({'username': 'admin'}, {'password': 'secret'}):
+        auth_req.params = params
+        with pytest.raises(ValueError):
+            do_login(auth_req)
+
